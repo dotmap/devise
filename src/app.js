d5ed1d680781900e4f81dc0db4c0fa9ac4225a2f
Graham Barber <grahammbarber@gmail.com>
2017-05-30 23:18:40 -0700

    Alright, let's try this with webpack

--- src/app.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-/* eslint-plugin-disable flowtype-errors */
-import Vue from 'vue'
-import App from './components/App.vue'
-
-const app = new Vue({ // eslint-disable-line
-  el: '#app',
-  render: (h) => h(App)
-})
