a15288d4c5ba585f44956bc2e71c60b9a69b320f
Graham Barber <grahammbarber@gmail.com>
2017-07-12 00:04:29 -0700

    What even is commitment? Rolled back flow, made e2e tests XO compliant

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,14 +1,15 @@
-import IndexPage from './page-model';
+/* global fixture test*/
+import IndexPage from './page-model'
 
-fixture `Index page`
-    .page('http://localhost:4000');
+fixture`Index page`
+  .page('http://localhost:4000')
 
-const page = new IndexPage();
+const page = new IndexPage()
 
 test('first test', async t => {
-    await t.expect(page.counter.innerText).contains('0')
-        .click(page.increment)
-        .expect(page.counter.innerText).contains('1')
-        .click(page.desrement)
-        .expect(page.counter.innerText).contains('0');
-});
+  await t.expect(page.counter.innerText).contains('0')
+    .click(page.increment)
+    .expect(page.counter.innerText).contains('1')
+    .click(page.desrement)
+    .expect(page.counter.innerText).contains('0')
+})
