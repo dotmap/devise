505c0fac0bca16213d2dd7e92e4f61efbb15e748
Graham Barber <grahammbarber@gmail.com>
2017-07-24 20:52:47 -0700

    :star2: (styles) Added SASS entry points
    With these inclusions, we now have access to Bulma's styles throughout the app. We can even override
    Bulma's default colors and variables to whatever we want.

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import './promise-polyfill'
 import { app } from './app'
 
+import './styles/vars.sass'
+
 // Enable progressive web app support (with offline-plugin)
 if (process.env.NODE_ENV === 'production') {
   require('./pwa')
