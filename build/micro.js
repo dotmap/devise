aff08a53d2d67b5dc5b8c789c52c5a9e30bffa50
Graham Barber <grahammbarber@gmail.com>
2017-07-24 18:29:10 -0700

    :art: (webpack) Converted express to micro
    Express is a weighty framework, and since I'm rewriting the webpack pipeline, I decided to rewrite
    the dev server as well so I understand it. It runs on zeit's micro framework now.

---
+++ build/micro.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const webpack = require('webpack')
+const { send } = require('micro')
+const webpackMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+
+const config = require('./webpack.dev')
+
+const compiler = webpack(config)
+const middleware = next => {
+  const mw = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  })
+  return (req, res) => mw(req, res, () => next(req, res))
+}
+
+const hotMiddleware = next => {
+  const mw = webpackHotMiddleware(compiler)
+  return (req, res) => mw(req, res, () => next(req, res))
+}
+
+const html = fs.readFileSync(`${__dirname}/index.html`, 'utf8')
+
+const microService = async (req, res) => send(res, 200, html)
+
+module.exports = [hotMiddleware, middleware].reduce(
+  (p, c) => c(p),
+  microService
+)
