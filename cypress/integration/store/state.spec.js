a3101a1c68898209863385ff38c3dee84dc90eb8
Graham Barber <grahammbarber@gmail.com>
2018-01-12 14:12:04 -0800

    Starting fresh

--- cypress/integration/store/state.spec.js
+++
@@ -1,8 +0,0 @@
-/* global describe it expect */
-import store from '../../../client/store'
-
-describe('Default State', () => {
-  it('Starts at count: 0', () => {
-    expect(store.state.count).to.equal(0)
-  })
-})
